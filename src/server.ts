5732e1a9ec44afe4d5950c0ea3e8d3a621534896
leonan <leonanorth3@gmail.com>
2025-07-10 00:10:45 -0300

    feat: add rooms listing

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import fastifyCors from '@fastify/cors'
+import fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider
+} from 'fastify-type-provider-zod'
+import { env } from './env.ts'
+import { getRoomsRoute } from './http/routes/get-rooms.ts'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+  origin: 'http://localhost:5173'
+})
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.get('/health', () => 'OK')
+
+app.register(getRoomsRoute)
+
+app
+  .listen({
+    port: env.PORT,
+    host: '0.0.0.0'
+  })
+  .then(() => {
+    console.log(`HTTP server running on http://localhost:${env.PORT}`)
+  })
